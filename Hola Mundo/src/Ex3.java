0c6bb5edafd37ca961f88765914368e4ce22d858
Ingrid <1415ingriddominguez@gmail.com>
2021-02-02 16:31:07 +0100

    Primer Commit

---
+++ Hola Mundo/src/Ex3.java
@@ -0,0 +1,14 @@
+
+public class Ex3 {
+
+	public static void main(String[] args) {
+		/*System.out.println("Hola");
+		 * System.out.println("Adios");
+		 * System.out.println("Cucu");*/
+		for (int i = 0; i < 6; i++)
+			System.out.print(i + " ");
+		System.out.println("FIN DE PROGRAMA");
+
+	}
+
+}
