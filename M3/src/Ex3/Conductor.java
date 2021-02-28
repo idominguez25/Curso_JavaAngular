df9d4337e9a71062c60ce1fc1e0524e62f1d67e0
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-02-28 19:56:15 +0100

    Delete M3/src/Ex3 directory

--- M3/src/Ex3/Conductor.java
+++
@@ -1,8 +0,0 @@
-package Ex3;
-
-public class Conductor extends Persona{
-	//Constructor
-	public Conductor(String nombre, String apellidos, String fechaNacimiento, String licencia, Licencia licencia1) {
-		super(nombre, apellidos, fechaNacimiento, licencia, licencia1);
-	}
-}
