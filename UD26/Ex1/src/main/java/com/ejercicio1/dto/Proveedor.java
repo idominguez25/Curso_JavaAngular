f1ae6198c39551c37570e1e136b1b351c6783a28
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 16:07:59 +0200

    Update Proveedor.java

--- UD26/Ex1/src/main/java/com/ejercicio1/dto/Proveedor.java
+++ UD26/Ex1/src/main/java/com/ejercicio1/dto/Proveedor.java
@@ -1,21 +1,11 @@
-/**
- * 
- */
 package com.ejercicio1.dto;
 
 import java.util.List;
-
 import javax.persistence.*;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-/**
- * @author Miguel A. Sastre
- *
- */
-
 @Entity
-@Table(name="proveedor")//en caso que la tabla sea diferente
+@Table(name="proveedor")
 public class Proveedor {
 	
 	@Id
@@ -26,58 +16,33 @@ public class Proveedor {
 	@JoinColumn(name = "id")
 	private List<Suministra> suministra;
 	
+	//Constructores
 	public Proveedor() {
 		
 	}
 
-	
-	/**
-	 * @param id
-	 * @param nombre
-	 * @param suministra
-	 */
 	public Proveedor(String id, String nombre, List<Suministra> suministra) {
 		this.id = id;
 		this.nombre = nombre;
 		this.suministra = suministra;
 	}
-
-
-	/**
-	 * @return the id
-	 */
+	
+	//Getters y Setters
 	public String getId() {
 		return id;
 	}
 
-
-	/**
-	 * @param id the id to set
-	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 
-
-	/**
-	 * @return the nombre
-	 */
 	public String getNombre() {
 		return nombre;
 	}
 
-
-	/**
-	 * @param nombre the nombre to set
-	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
-
-	/**
-	 * @return the suministra
-	 */
 
 	@JsonIgnore
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "Suministra")
@@ -85,14 +50,11 @@ public class Proveedor {
 		return suministra;
 	}
 
-	/**
-	 * @param suministra the suministra to set
-	 */
 	public void setSuministra(List<Suministra> suministra) {
 		this.suministra = suministra;
 	}
 
-
+	//toString
 	@Override
 	public String toString() {
 		return "Proveedor [id=" + id + ", nombre=" + nombre + ", suministra=" + suministra + "]";
