feef6d610a4a039f0eada709c5a5f80ad0c1dcee
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 18:50:57 +0200

    Update CajeroServiceImpl.java

--- UD26/Ex3/src/main/java/com/ejercicio3/service/CajeroServiceImpl.java
+++ UD26/Ex3/src/main/java/com/ejercicio3/service/CajeroServiceImpl.java
@@ -1,20 +1,11 @@
-/**
- * 
- */
 package com.ejercicio3.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.ejercicio3.dao.ICajeroDao;
 import com.ejercicio3.dto.Cajero;
 
-/**
- * @author Miguel A. Sastre
- *
- */
 @Service
 public class CajeroServiceImpl implements ICajeroService{
 	
