08ca49ef328bdb72483d787e64bcab283315cbdf
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 15:21:56 +0200

    Update IEmpleadoDao.java

--- UD25/Ex2/src/main/java/com/ejercicio2/dao/IEmpleadoDao.java
+++ UD25/Ex2/src/main/java/com/ejercicio2/dao/IEmpleadoDao.java
@@ -1,16 +1,8 @@
-/**
- * 
- */
 package com.ejercicio2.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ejercicio2.dto.Empleado;
 
-/**
- * @author Miguel A. Sastre
- *
- */
 public interface IEmpleadoDao extends JpaRepository<Empleado, String> {
 
 }
