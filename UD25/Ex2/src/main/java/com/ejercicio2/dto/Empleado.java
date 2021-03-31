dac4cbd456dfb13b745d5b47b3b7b2f41cc3eaff
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 15:25:31 +0200

    Update Empleado.java

--- UD25/Ex2/src/main/java/com/ejercicio2/dto/Empleado.java
+++ UD25/Ex2/src/main/java/com/ejercicio2/dto/Empleado.java
@@ -1,16 +1,9 @@
-/**
- * 
- */
 package com.ejercicio2.dto;
 
 import javax.persistence.*;
 
-/**
- * @author Miguel A. Sastre
- *
- */
 @Entity
-@Table(name="empleado")//en caso que la tabla sea diferente
+@Table(name="empleado")
 public class Empleado {
 	
 	@Id
@@ -23,20 +16,10 @@ public class Empleado {
 	@JoinColumn(name="departamento_id")
 	private Departamento departamento;
 	
-	/**
-	 * Constructor por defecto 
-	 */
-	
-	public Empleado() { }
+	//Constructores
+	public Empleado() {
+	}
 
-	
-	
-	/**
-	 * @param dni
-	 * @param nombre
-	 * @param apellido
-	 * @param departamento
-	 */
 	public Empleado(String dni, String nombre, String apellido, Departamento departamento) {
 		super();
 		this.dni = dni;
@@ -45,80 +28,40 @@ public class Empleado {
 		this.departamento = departamento;
 	}
 
-
-
-	/**
-	 * @return the dni
-	 */
+	//Getters y Setters
 	public String getDni() {
 		return dni;
 	}
 
-
-
-	/**
-	 * @param dni the dni to set
-	 */
 	public void setDni(String dni) {
 		this.dni = dni;
 	}
 
-
-
-	/**
-	 * @return the nombre
-	 */
 	public String getNombre() {
 		return nombre;
 	}
 
-
-
-	/**
-	 * @param nombre the nombre to set
-	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
-
-
-	/**
-	 * @return the apellido
-	 */
 	public String getApellido() {
 		return apellidos;
 	}
 
-
-
-	/**
-	 * @param apellido the apellido to set
-	 */
 	public void setApellido(String apellido) {
 		this.apellidos = apellido;
 	}
 
-
-
-	/**
-	 * @return the departamento
-	 */
 	public Departamento getDepartamento() {
 		return departamento;
 	}
 
-
-
-	/**
-	 * @param departamento the departamento to set
-	 */
 	public void setDepartamento(Departamento departamento) {
 		this.departamento = departamento;
 	}
 
-
-
+	//toString
 	@Override
 	public String toString() {
 		return "Empleado [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellidos + ", departamento="
