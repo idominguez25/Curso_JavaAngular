6b4ffc0706fc0adc1e891c73488b6413222ca11b
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 17:38:41 +0200

    Update ICajeroDao.java

--- UD26/Ex3/src/main/java/com/ejercicio3/dao/ICajeroDao.java
+++ UD26/Ex3/src/main/java/com/ejercicio3/dao/ICajeroDao.java
@@ -1,17 +1,8 @@
-/**
- * 
- */
 package com.ejercicio3.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ejercicio3.dto.Cajero;
 
-
-/**
- * @author Miguel A. Sastre
- *
- */
 public interface ICajeroDao extends JpaRepository<Cajero, Long> {
 
 }
