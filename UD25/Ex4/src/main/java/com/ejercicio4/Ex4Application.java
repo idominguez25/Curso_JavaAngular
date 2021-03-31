df669a8cca8d18dee981cf4c767ac285689900df
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 15:53:19 +0200

    Update and rename Ud25Ejercicio4Application.java to Ex4Application.java

--- UD25/Ex4/src/main/java/com/ejercicio4/Ex4Application.java
+++ UD25/Ex4/src/main/java/com/ejercicio4/Ex4Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ud25Ejercicio4Application {
+public class Ex4Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ud25Ejercicio4Application.class, args);
+		SpringApplication.run(Ex4Application.class, args);
 	}
 
 }
