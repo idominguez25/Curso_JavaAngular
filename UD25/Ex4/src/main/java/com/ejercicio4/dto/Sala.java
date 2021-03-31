a5d86be84d51c528f40d8c5951ffa199f1235338
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 15:49:59 +0200

    Update Sala.java

--- UD25/Ex4/src/main/java/com/ejercicio4/dto/Sala.java
+++ UD25/Ex4/src/main/java/com/ejercicio4/dto/Sala.java
@@ -1,16 +1,9 @@
-/**
- * 
- */
 package com.ejercicio4.dto;
 
 import javax.persistence.*;
 
-/**
- * @author Miguel A. Sastre
- *
- */
 @Entity
-@Table(name="sala")//en caso que la tabla sea diferente
+@Table(name="sala")
 public class Sala {
 	
 	@Id
@@ -25,65 +18,43 @@ public class Sala {
 	@JoinColumn(name="pelicula_id")
 	private Pelicula pelicula;
 	
-	/**
-	 * Constructor por defecto 
-	 */
+	//Constructores
 	
-	public Sala() { }
+	public Sala() {
+	}
 
-	/**
-	 * @param codigo
-	 * @param nombre
-	 * @param pelicula
-	 */
 	public Sala(Long codigo, String nombre, Pelicula pelicula) {
 		this.codigo = codigo;
 		this.nombre = nombre;
 		this.pelicula = pelicula;
 	}
 
-	/**
-	 * @return the codigo
-	 */
+	//Getters y Setters
 	public Long getCodigo() {
 		return codigo;
 	}
 
-	/**
-	 * @param codigo the codigo to set
-	 */
 	public void setCodigo(Long codigo) {
 		this.codigo = codigo;
 	}
-
-	/**
-	 * @return the nombre
-	 */
+	
 	public String getNombre() {
 		return nombre;
 	}
 
-	/**
-	 * @param nombre the nombre to set
-	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
-	/**
-	 * @return the pelicula
-	 */
 	public Pelicula getPelicula() {
 		return pelicula;
 	}
 
-	/**
-	 * @param pelicula the pelicula to set
-	 */
 	public void setPelicula(Pelicula pelicula) {
 		this.pelicula = pelicula;
 	}
 
+	//toString
 	@Override
 	public String toString() {
 		return "Sala [codigo=" + codigo + ", nombre=" + nombre + ", pelicula=" + pelicula + "]";
