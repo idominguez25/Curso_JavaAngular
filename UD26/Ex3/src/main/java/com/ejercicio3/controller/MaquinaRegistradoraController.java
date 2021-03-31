81c8c2ad333a9b4c71009d4d2260c7e488e94ce1
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 17:37:09 +0200

    Update MaquinaRegistradoraController.java

--- UD26/Ex3/src/main/java/com/ejercicio3/controller/MaquinaRegistradoraController.java
+++ UD26/Ex3/src/main/java/com/ejercicio3/controller/MaquinaRegistradoraController.java
@@ -1,20 +1,10 @@
-/**
- * 
- */
 package com.ejercicio3.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import com.ejercicio3.dto.MaquinaRegistradora;
 import com.ejercicio3.service.MaquinaRegistradoraServiceImpl;
-
-/**
- * @author Miguel A. Sastre
- *
- */
 
 @RestController
 @RequestMapping("/api")
