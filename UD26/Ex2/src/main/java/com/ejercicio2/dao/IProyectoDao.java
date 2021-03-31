ce62611d2293c27cfb4d992c0db961edad5b42f2
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 16:15:12 +0200

    Update IProyectoDao.java

--- UD26/Ex2/src/main/java/com/ejercicio2/dao/IProyectoDao.java
+++ UD26/Ex2/src/main/java/com/ejercicio2/dao/IProyectoDao.java
@@ -1,17 +1,8 @@
-/**
- * 
- */
 package com.ejercicio2.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ejercicio2.dto.Proyecto;
 
-
-/**
- * @author Miguel A. Sastre
- *
- */
 public interface IProyectoDao extends JpaRepository<Proyecto, String> {
 
 }
