024fc8e7810041e4bd1efe60d910e85d0fc8ccbc
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 16:16:28 +0200

    Update AsignadoA.java

--- UD26/Ex2/src/main/java/com/ejercicio2/dto/AsignadoA.java
+++ UD26/Ex2/src/main/java/com/ejercicio2/dto/AsignadoA.java
@@ -1,16 +1,9 @@
-/**
- * 
- */
 package com.ejercicio2.dto;
 
 import javax.persistence.*;
 
-/**
- * @author Miguel A. Sastre
- *
- */
 @Entity
-@Table(name="asignadoa")//en caso que la tabla sea diferente
+@Table(name="asignadoa")
 public class AsignadoA {
 	
 	@Id
@@ -26,47 +19,28 @@ public class AsignadoA {
 	@JoinColumn(name="proyecto_id")
 	private Proyecto proyecto;
 	
-	/**
-	 * Constructor por defecto 
-	 */
-	
-	public AsignadoA() { }
-	
-	
-	/**
-	 * @param cientifico
-	 * @param proyecto
-	 */
+	//Constructores
+	public AsignadoA() {
+	}
+
 	public AsignadoA(Cientifico cientifico, Proyecto proyecto) {
 		this.cientifico = cientifico;
 		this.proyecto = proyecto;
 	}
 
-
-	/**
-	 * @return the cientifico
-	 */
+	//Getters y Setters
 	public Cientifico getCientifico() {
 		return cientifico;
 	}
 
-	/**
-	 * @param cientifico the cientifico to set
-	 */
 	public void setCientifico(Cientifico cientifico) {
 		this.cientifico = cientifico;
 	}
 
-	/**
-	 * @return the proyecto
-	 */
 	public Proyecto getProyecto() {
 		return proyecto;
 	}
 
-	/**
-	 * @param proyecto the proyecto to set
-	 */
 	public void setProyecto(Proyecto proyecto) {
 		this.proyecto = proyecto;
 	}
