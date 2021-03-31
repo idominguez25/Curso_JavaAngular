1b44da508cce52a0ffdc6e98a950a2b1a7254424
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 19:05:42 +0200

    Update Investigador.java

--- UD26/Ex4/src/main/java/com/ejercicio4/dto/Investigador.java
+++ UD26/Ex4/src/main/java/com/ejercicio4/dto/Investigador.java
@@ -1,21 +1,11 @@
-/**
- * 
- */
 package com.ejercicio4.dto;
 
 import java.util.List;
-
 import javax.persistence.*;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-/**
- * @author Miguel A. Sastre
- *
- */
-
 @Entity
-@Table(name="investigador")//en caso que la tabla sea diferente
+@Table(name="investigador")
 public class Investigador {
 	
 	@Id
@@ -30,19 +20,10 @@ public class Investigador {
 	@JoinColumn(name="investigador_facultad_id")
 	private Facultad facultad;
 	
+	//Constructores
 	public Investigador() {
-		
 	}
 	
-	
-
-	/**
-	 * @param dni
-	 * @param nomapels
-	 * @param precio
-	 * @param reserva
-	 * @param facultad
-	 */
 	public Investigador(String dni, String nomapels, List<Reserva> reserva, Facultad facultad) {
 		this.dni = dni;
 		this.nomapels = nomapels;
@@ -50,47 +31,22 @@ public class Investigador {
 		this.facultad = facultad;
 	}
 
-
-	
-
-	/**
-	 * @return the dni
-	 */
+	//Getters y Setters
 	public String getDni() {
 		return dni;
 	}
-
-
-
-	/**
-	 * @param dni the dni to set
-	 */
+	
 	public void setDni(String dni) {
 		this.dni = dni;
 	}
 
-
-
-	/**
-	 * @return the nomapels
-	 */
 	public String getNomapels() {
 		return nomapels;
 	}
 
-
-
-	/**
-	 * @param nomapels the nomapels to set
-	 */
 	public void setNomapels(String nomapels) {
 		this.nomapels = nomapels;
 	}
-
-
-	/**
-	 * @return the reserva
-	 */
 	
 	@JsonIgnore
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "Reserva")
@@ -98,34 +54,19 @@ public class Investigador {
 		return reserva;
 	}
 
-
-	/**
-	 * @param reserva the reserva to set
-	 */
 	public void setReserva(List<Reserva> reserva) {
 		this.reserva = reserva;
 	}
 
-
-
-	/**
-	 * @return the facultad
-	 */
 	public Facultad getFacultad() {
 		return facultad;
 	}
 
-
-
-	/**
-	 * @param facultad the facultad to set
-	 */
 	public void setFacultad(Facultad facultad) {
 		this.facultad = facultad;
 	}
 
-
-
+	//toString
 	@Override
 	public String toString() {
 		return "Investigador [dni=" + dni + ", nomapels=" + nomapels + ", reserva=" + reserva + ", facultad="
