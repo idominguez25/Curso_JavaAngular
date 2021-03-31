4861ff921152506cb32f16688f47e34565d24bbb
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 16:22:42 +0200

    .

--- UD26/Ex2/src/main/java/com/ejercicio2/Ex2Application.java
+++ UD26/Ex2/src/main/java/com/ejercicio2/Ex2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ud26Ejercicio2Application {
+public class Ex2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ud26Ejercicio2Application.class, args);
+		SpringApplication.run(Ex2Application.class, args);
 	}
 
 }
