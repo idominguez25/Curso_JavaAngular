aa6020ce1513f75985883955ccd926da6919f86f
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 15:35:01 +0200

    Update AlmacenController.java

--- UD25/Ex3/src/main/java/com/ejercicio3/controller/AlmacenController.java
+++ UD25/Ex3/src/main/java/com/ejercicio3/controller/AlmacenController.java
@@ -1,20 +1,10 @@
-/**
- * 
- */
 package com.ejercicio3.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import com.ejercicio3.dto.Almacen;
 import com.ejercicio3.service.AlmacenServiceImpl;
-
-/**
- * @author Miguel A. Sastre
- *
- */
 
 @RestController
 @RequestMapping("/api")
