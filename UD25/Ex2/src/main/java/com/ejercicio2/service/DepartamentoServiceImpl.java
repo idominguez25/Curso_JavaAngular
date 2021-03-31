6663795653086caf70c817868a244dee269ca5d4
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 15:30:04 +0200

    Update DepartamentoServiceImpl.java

--- UD25/Ex2/src/main/java/com/ejercicio2/service/DepartamentoServiceImpl.java
+++ UD25/Ex2/src/main/java/com/ejercicio2/service/DepartamentoServiceImpl.java
@@ -1,20 +1,11 @@
-/**
- * 
- */
 package com.ejercicio2.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.ejercicio2.dao.IDepartamentoDao;
 import com.ejercicio2.dto.Departamento;
 
-/**
- * @author Miguel A. Sastre
- *
- */
 @Service
 public class DepartamentoServiceImpl implements IDepartamentoService{
 	
