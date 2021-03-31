867c3996ef67dfc6f87e0cdc37283e4d010de12f
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 16:03:20 +0200

    Update IPiezaDao.java

--- UD26/Ex1/src/main/java/com/ejercicio1/dao/IPiezaDao.java
+++ UD26/Ex1/src/main/java/com/ejercicio1/dao/IPiezaDao.java
@@ -1,17 +1,8 @@
-/**
- * 
- */
 package com.ejercicio1.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ejercicio1.dto.Pieza;
 
-
-/**
- * @author Miguel A. Sastre
- *
- */
 public interface IPiezaDao extends JpaRepository<Pieza, Long> {
 
 }
