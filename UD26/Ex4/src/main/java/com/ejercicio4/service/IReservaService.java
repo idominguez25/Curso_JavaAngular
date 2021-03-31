48c297f58c9fa0169cbc3c3e278314779c1016d1
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 19:09:12 +0200

    Update IReservaService.java

--- UD26/Ex4/src/main/java/com/ejercicio4/service/IReservaService.java
+++ UD26/Ex4/src/main/java/com/ejercicio4/service/IReservaService.java
@@ -1,17 +1,9 @@
-/**
- * 
- */
 package com.ejercicio4.service;
 
 import java.util.List;
-
 import com.ejercicio4.dto.Equipo;
 import com.ejercicio4.dto.Reserva;
 
-/**
- * @author Miguel A. Sastre
- *
- */
 public interface IReservaService {
 	
 	public List<Reserva> listarReserva();
