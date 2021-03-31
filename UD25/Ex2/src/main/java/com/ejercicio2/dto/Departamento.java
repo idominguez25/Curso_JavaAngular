0d6c0ad8ccd34ef6dc5d236ff1fa518ee2a1b473
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 15:23:56 +0200

    Update Departamento.java

--- UD25/Ex2/src/main/java/com/ejercicio2/dto/Departamento.java
+++ UD25/Ex2/src/main/java/com/ejercicio2/dto/Departamento.java
@@ -1,21 +1,11 @@
-/**
- * 
- */
 package com.ejercicio2.dto;
 
 import java.util.List;
-
 import javax.persistence.*;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-/**
- * @author Miguel A. Sastre
- *
- */
-
 @Entity
-@Table(name="departamento")//en caso que la tabla sea diferente
+@Table(name="departamento")
 public class Departamento {
 	
 	@Id
@@ -27,18 +17,10 @@ public class Departamento {
 	@JoinColumn(name = "dni")
 	private List<Empleado> empleado;
 	
+	//Constructores
 	public Departamento() {
-		
 	}
-	
-	
-	
-	/**
-	 * @param codigo
-	 * @param nombre
-	 * @param presupuesto
-	 * @param empleado
-	 */
+
 	public Departamento(Long codigo, String nombre, Double presupuesto, List<Empleado> empleado) {
 		super();
 		this.codigo = codigo;
@@ -47,68 +29,42 @@ public class Departamento {
 		this.empleado = empleado;
 	}
 
-
-
-	/**
-	 * @return the codigo
-	 */
+	//Getters y Setters
 	public Long getCodigo() {
 		return codigo;
 	}
 
-	/**
-	 * @param codigo the codigo to set
-	 */
 	public void setCodigo(Long codigo) {
 		this.codigo = codigo;
 	}
-
-	/**
-	 * @return the nombre
-	 */
+	
 	public String getNombre() {
 		return nombre;
 	}
 
-	/**
-	 * @param nombre the nombre to set
-	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
-	/**
-	 * @return the presupuesto
-	 */
 	public Double getPresupuesto() {
 		return presupuesto;
 	}
 
-	/**
-	 * @param presupuesto the presupuesto to set
-	 */
 	public void setPresupuesto(Double presupuesto) {
 		this.presupuesto = presupuesto;
 	}
 
-	/**
-	 * @return the empleado
-	 */
 	@JsonIgnore
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "Empleado")
 	public List<Empleado> getEmpleado() {
 		return empleado;
 	}
 
-	/**
-	 * @param empleado the empleado to set
-	 */
 	public void setEmpleado(List<Empleado> empleado) {
 		this.empleado = empleado;
 	}
 
-
-
+	//toString
 	@Override
 	public String toString() {
 		return "Departamento [codigo=" + codigo + ", nombre=" + nombre + ", presupuesto=" + presupuesto + ", empleado="
