9d0f1d25f6b02ddc6a7137d75e02c8d34ff07b84
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 19:11:59 +0200

    .

--- UD26/Ex4/src/main/java/com/ejercicio4/Ex4Application.java
+++ UD26/Ex4/src/main/java/com/ejercicio4/Ex4Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ud26Ejercicio4Application {
+public class Ex4Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ud26Ejercicio4Application.class, args);
+		SpringApplication.run(Ex4Application.class, args);
 	}
 
 }
