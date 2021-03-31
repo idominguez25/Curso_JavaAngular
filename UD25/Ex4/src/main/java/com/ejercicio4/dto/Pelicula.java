9daf8c7fbc6e4230a132b6d6ddec1f960f9c9949
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 15:48:29 +0200

    Update Pelicula.java

--- UD25/Ex4/src/main/java/com/ejercicio4/dto/Pelicula.java
+++ UD25/Ex4/src/main/java/com/ejercicio4/dto/Pelicula.java
@@ -1,21 +1,11 @@
-/**
- * 
- */
 package com.ejercicio4.dto;
 
 import java.util.List;
-
 import javax.persistence.*;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-/**
- * @author Miguel A. Sastre
- *
- */
-
 @Entity
-@Table(name="pelicula")//en caso que la tabla sea diferente
+@Table(name="pelicula")
 public class Pelicula {
 	
 	@Id
@@ -31,18 +21,10 @@ public class Pelicula {
 	@JoinColumn(name = "codigo")
 	private List<Sala> sala;
 	
-	public Pelicula() {
-		
+	//Constructores
+	public Pelicula() {	
 	}
 	
-	
-	
-	/**
-	 * @param codigo
-	 * @param nombre
-	 * @param calificacionEdad
-	 * @param sala
-	 */
 	public Pelicula(Long codigo, String nombre, Long calificacionEdad, List<Sala> sala) {
 		this.codigo = codigo;
 		this.nombre = nombre;
@@ -50,68 +32,42 @@ public class Pelicula {
 		this.sala = sala;
 	}
 
-	/**
-	 * @return the codigo
-	 */
+	//Getters y Setters
 	public Long getCodigo() {
 		return codigo;
 	}
 
-	/**
-	 * @param codigo the codigo to set
-	 */
 	public void setCodigo(Long codigo) {
 		this.codigo = codigo;
 	}
 
-	/**
-	 * @return the nombre
-	 */
 	public String getNombre() {
 		return nombre;
 	}
 
-	/**
-	 * @param nombre the nombre to set
-	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
-	/**
-	 * @return the calificacionEdad
-	 */
+	
 	public Long getCalificacionEdad() {
 		return calificacionEdad;
 	}
 
-	/**
-	 * @param calificacionEdad the calificacionEdad to set
-	 */
 	public void setCalificacionEdad(Long calificacionEdad) {
 		this.calificacionEdad = calificacionEdad;
 	}
 
-	/**
-	 * @return the sala
-	 */
 	@JsonIgnore
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "Sala")
 	public List<Sala> getSala() {
 		return sala;
 	}
 
-	/**
-	 * @param sala the sala to set
-	 */
 	public void setSala(List<Sala> sala) {
 		this.sala = sala;
 	}
 
-	
-
-
-
+	//toString
 	@Override
 	public String toString() {
 		return "Pelicula [codigo=" + codigo + ", nombre=" + nombre + ", calificacionEdad=" + calificacionEdad
