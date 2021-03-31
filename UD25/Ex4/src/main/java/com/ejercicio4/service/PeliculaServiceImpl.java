2b043175722e940d7a520114012b60de99640459
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 15:51:30 +0200

    Update PeliculaServiceImpl.java

--- UD25/Ex4/src/main/java/com/ejercicio4/service/PeliculaServiceImpl.java
+++ UD25/Ex4/src/main/java/com/ejercicio4/service/PeliculaServiceImpl.java
@@ -1,20 +1,11 @@
-/**
- * 
- */
 package com.ejercicio4.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.ejercicio4.dao.IPeliculaDao;
 import com.ejercicio4.dto.Pelicula;
 
-/**
- * @author Miguel A. Sastre
- *
- */
 @Service
 public class PeliculaServiceImpl implements IPeliculaService{
 	
