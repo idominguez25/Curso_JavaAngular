4f059f18deda0f3f775bab2566db59652856e965
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 15:13:01 +0200

    Update and rename Ud25Ejercicio1Application.java to Ex1Application.java

--- UD25/Ex1/src/main/java/com/ejercicio1/Ex1Application.java
+++ UD25/Ex1/src/main/java/com/ejercicio1/Ex1Application.java
@@ -1,10 +1,10 @@
-package com.ejercicio1;
+package com.Ex1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ud25Ejercicio1Application {
+public class Ex1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ud25Ejercicio1Application.class, args);
