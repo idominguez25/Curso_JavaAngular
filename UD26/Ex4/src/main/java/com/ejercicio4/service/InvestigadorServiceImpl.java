29d5173e02ade9341d4ba22925386efa4c178332
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 19:09:55 +0200

    Update InvestigadorServiceImpl.java

--- UD26/Ex4/src/main/java/com/ejercicio4/service/InvestigadorServiceImpl.java
+++ UD26/Ex4/src/main/java/com/ejercicio4/service/InvestigadorServiceImpl.java
@@ -1,22 +1,13 @@
-/**
- * 
- */
 package com.ejercicio4.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.ejercicio4.dao.IEquipoDao;
 import com.ejercicio4.dao.IInvestigadorDao;
 import com.ejercicio4.dto.Equipo;
 import com.ejercicio4.dto.Investigador;
 
-/**
- * @author Miguel A. Sastre
- *
- */
 @Service
 public class InvestigadorServiceImpl implements IInvestigadorService{
 	
