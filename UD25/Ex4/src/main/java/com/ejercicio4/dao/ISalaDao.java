6b509c0c1b8620acf0a36ca82d28b1fabcb8e573
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 15:47:15 +0200

    Update ISalaDao.java

--- UD25/Ex4/src/main/java/com/ejercicio4/dao/ISalaDao.java
+++ UD25/Ex4/src/main/java/com/ejercicio4/dao/ISalaDao.java
@@ -1,16 +1,8 @@
-/**
- * 
- */
 package com.ejercicio4.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ejercicio4.dto.Sala;
 
-/**
- * @author Miguel A. Sastre
- *
- */
 public interface ISalaDao extends JpaRepository<Sala, Long> {
 
 }
