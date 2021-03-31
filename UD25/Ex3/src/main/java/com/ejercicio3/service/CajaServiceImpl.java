b2763532c05a6168dcf144ec4c50f65e54037845
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 15:41:02 +0200

    Update CajaServiceImpl.java

--- UD25/Ex3/src/main/java/com/ejercicio3/service/CajaServiceImpl.java
+++ UD25/Ex3/src/main/java/com/ejercicio3/service/CajaServiceImpl.java
@@ -1,20 +1,11 @@
-/**
- * 
- */
 package com.ejercicio3.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.ejercicio3.dao.ICajaDao;
 import com.ejercicio3.dto.Caja;
 
-/**
- * @author Miguel A. Sastre
- *
- */
 @Service
 public class CajaServiceImpl implements ICajaService {
 
