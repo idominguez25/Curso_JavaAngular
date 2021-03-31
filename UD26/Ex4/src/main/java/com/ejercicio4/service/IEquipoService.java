ff6b1c75033f4879af9b0c7692eed5e76c1ab8c8
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 19:08:29 +0200

    Update IEquipoService.java

--- UD26/Ex4/src/main/java/com/ejercicio4/service/IEquipoService.java
+++ UD26/Ex4/src/main/java/com/ejercicio4/service/IEquipoService.java
@@ -1,16 +1,8 @@
-/**
- * 
- */
 package com.ejercicio4.service;
 
 import java.util.List;
-
 import com.ejercicio4.dto.Equipo;
 
-/**
- * @author Miguel A. Sastre
- *
- */
 public interface IEquipoService {
 	
 	public List<Equipo> listarEquipos();
