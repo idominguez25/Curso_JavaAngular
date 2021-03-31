68938c8490139b92add7780c290c634d3243230f
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 19:07:40 +0200

    Update Reserva.java

--- UD26/Ex4/src/main/java/com/ejercicio4/dto/Reserva.java
+++ UD26/Ex4/src/main/java/com/ejercicio4/dto/Reserva.java
@@ -1,18 +1,10 @@
-/**
- * 
- */
 package com.ejercicio4.dto;
 
 import java.util.Date;
-
 import javax.persistence.*;
 
-/**
- * @author Miguel A. Sastre
- *
- */
 @Entity
-@Table(name="reserva")//en caso que la tabla sea diferente
+@Table(name="reserva")
 public class Reserva {
 	
 	@Id
@@ -31,20 +23,10 @@ public class Reserva {
 	private Investigador investigador;
 	
 	
-	/**
-	 * Constructor por defecto 
-	 */
-	
-	public Reserva() { }
+	//Constructores
+	public Reserva() {
+	}
 
-
-	/**
-	 * @param id
-	 * @param comienzo
-	 * @param fin
-	 * @param equipo
-	 * @param investigador
-	 */
 	public Reserva(int id, Date comienzo, Date fin, Equipo equipo, Investigador investigador) {
 		super();
 		this.id = id;
@@ -55,91 +37,51 @@ public class Reserva {
 	}
 
 
-	/**
-	 * @return the id
-	 */
+	//Getters y Setters
 	public int getId() {
 		return id;
 	}
 
-
-	/**
-	 * @param id the id to set
-	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
-	/**
-	 * @return the comienzo
-	 */
 	public Date getComienzo() {
 		return comienzo;
 	}
 
-
-	/**
-	 * @param comienzo the comienzo to set
-	 */
 	public void setComienzo(Date comienzo) {
 		this.comienzo = comienzo;
 	}
 
-
-	/**
-	 * @return the fin
-	 */
 	public Date getFin() {
 		return fin;
 	}
 
-
-	/**
-	 * @param fin the fin to set
-	 */
 	public void setFin(Date fin) {
 		this.fin = fin;
 	}
 
-
-	/**
-	 * @return the equipo
-	 */
 	public Equipo getEquipos() {
 		return equipo;
 	}
 
-
-	/**
-	 * @param equipo the equipo to set
-	 */
 	public void setEquipos(Equipo equipo) {
 		this.equipo = equipo;
 	}
 
-
-	/**
-	 * @return the investigador
-	 */
 	public Investigador getInvestigadores() {
 		return investigador;
 	}
 
-
-	/**
-	 * @param investigador the investigador to set
-	 */
 	public void setInvestigadores(Investigador investigador) {
 		this.investigador = investigador;
 	}
 
-
+	//toString
 	@Override
 	public String toString() {
 		return "Reserva [id=" + id + ", comienzo=" + comienzo + ", fin=" + fin + ", equipo=" + equipo
 				+ ", investigador=" + investigador + "]";
-	}
-
-	
+	}	
 }
