e29462bf2de2a3e1b8e7a72935376244ba21bcbd
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 19:07:59 +0200

    Update EquipoServiceImpl.java

--- UD26/Ex4/src/main/java/com/ejercicio4/service/EquipoServiceImpl.java
+++ UD26/Ex4/src/main/java/com/ejercicio4/service/EquipoServiceImpl.java
@@ -1,20 +1,11 @@
-/**
- * 
- */
 package com.ejercicio4.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.ejercicio4.dao.IEquipoDao;
 import com.ejercicio4.dto.Equipo;
 
-/**
- * @author Miguel A. Sastre
- *
- */
 @Service
 public class EquipoServiceImpl implements IEquipoService{
 	
