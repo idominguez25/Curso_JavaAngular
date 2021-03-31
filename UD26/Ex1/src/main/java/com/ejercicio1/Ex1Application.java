2051ae8f22fa63f519c2af1eefaa608b1a8fabba
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 16:12:45 +0200

    .

--- UD26/Ex1/src/main/java/com/ejercicio1/Ex1Application.java
+++ UD26/Ex1/src/main/java/com/ejercicio1/Ex1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ud26Ejercicio1Application {
+public class Ex1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ud26Ejercicio1Application.class, args);
+		SpringApplication.run(Ex1Application.class, args);
 	}
 
 }
