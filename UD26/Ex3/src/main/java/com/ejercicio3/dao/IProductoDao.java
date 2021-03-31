6935769e2ce09ba44fcfd1807db589b34795f1be
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 17:39:17 +0200

    Update IProductoDao.java

--- UD26/Ex3/src/main/java/com/ejercicio3/dao/IProductoDao.java
+++ UD26/Ex3/src/main/java/com/ejercicio3/dao/IProductoDao.java
@@ -1,17 +1,8 @@
-/**
- * 
- */
 package com.ejercicio3.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ejercicio3.dto.Producto;
 
-
-/**
- * @author Miguel A. Sastre
- *
- */
 public interface IProductoDao extends JpaRepository<Producto, Long> {
 
 }
