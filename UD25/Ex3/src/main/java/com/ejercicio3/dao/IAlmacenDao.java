944e5eb638904f4694dc64d3f573b8a37787e4a8
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 15:35:37 +0200

    Update IAlmacenDao.java

--- UD25/Ex3/src/main/java/com/ejercicio3/dao/IAlmacenDao.java
+++ UD25/Ex3/src/main/java/com/ejercicio3/dao/IAlmacenDao.java
@@ -1,17 +1,8 @@
-/**
- * 
- */
 package com.ejercicio3.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ejercicio3.dto.Almacen;
 
-
-/**
- * @author Miguel A. Sastre
- *
- */
 public interface IAlmacenDao extends JpaRepository<Almacen, Long> {
 
 }
