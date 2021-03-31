8d48dc968628ff4d1e49ccc84e6c57c134f7a1d1
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 18:58:19 +0200

    Update IEquipoDao.java

--- UD26/Ex4/src/main/java/com/ejercicio4/dao/IEquipoDao.java
+++ UD26/Ex4/src/main/java/com/ejercicio4/dao/IEquipoDao.java
@@ -1,17 +1,8 @@
-/**
- * 
- */
 package com.ejercicio4.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ejercicio4.dto.Equipo;
 
-
-/**
- * @author Miguel A. Sastre
- *
- */
 public interface IEquipoDao extends JpaRepository<Equipo, String> {
 
 }
