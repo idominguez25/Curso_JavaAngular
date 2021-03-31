1c3d77eb179d4dd947b6a83499b39d0f726f534e
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 15:30:54 +0200

    Update IEmpleadoService.java

--- UD25/Ex2/src/main/java/com/ejercicio2/service/IEmpleadoService.java
+++ UD25/Ex2/src/main/java/com/ejercicio2/service/IEmpleadoService.java
@@ -1,17 +1,9 @@
-/**
- * 
- */
 package com.ejercicio2.service;
 
 import java.util.List;
-
 import com.ejercicio2.dto.Empleado;
 import com.ejercicio2.dto.Departamento;
 
-/**
- * @author Miguel A. Sastre
- *
- */
 public interface IEmpleadoService {
 	
 	public List<Empleado> listarEmpleados();
