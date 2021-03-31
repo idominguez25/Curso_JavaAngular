a92caf89d1d9bc44e8f053574a7b86ba31619d0b
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 16:03:37 +0200

    Update IProveedorDao.java

--- UD26/Ex1/src/main/java/com/ejercicio1/dao/IProveedorDao.java
+++ UD26/Ex1/src/main/java/com/ejercicio1/dao/IProveedorDao.java
@@ -1,17 +1,8 @@
-/**
- * 
- */
 package com.ejercicio1.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ejercicio1.dto.Proveedor;
 
-
-/**
- * @author Miguel A. Sastre
- *
- */
 public interface IProveedorDao extends JpaRepository<Proveedor, String> {
 
 }
