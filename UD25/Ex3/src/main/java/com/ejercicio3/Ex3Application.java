608e9a1ceeef5db6cbe1829de7621e460aa39185
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 15:42:08 +0200

    Update and rename Ud25Ejercicio3Application.java to Ex3Application.java

--- UD25/Ex3/src/main/java/com/ejercicio3/Ex3Application.java
+++ UD25/Ex3/src/main/java/com/ejercicio3/Ex3Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ud25Ejercicio3Application {
+public class Ex3Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ud25Ejercicio3Application.class, args);
+		SpringApplication.run(Ex3Application.class, args);
 	}
 
 }
