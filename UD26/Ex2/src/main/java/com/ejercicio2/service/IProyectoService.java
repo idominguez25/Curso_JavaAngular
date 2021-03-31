11e5088a92d5e5a422a55098f19b580f3d048c0b
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 16:21:14 +0200

    Update IProyectoService.java

--- UD26/Ex2/src/main/java/com/ejercicio2/service/IProyectoService.java
+++ UD26/Ex2/src/main/java/com/ejercicio2/service/IProyectoService.java
@@ -1,16 +1,8 @@
-/**
- * 
- */
 package com.ejercicio2.service;
 
 import java.util.List;
-
 import com.ejercicio2.dto.Proyecto;
 
-/**
- * @author Miguel A. Sastre
- *
- */
 public interface IProyectoService {
 	
 	public List<Proyecto> listarProyectos();
