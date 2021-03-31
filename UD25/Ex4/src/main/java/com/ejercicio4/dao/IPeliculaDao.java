72cb587a7cccafe7a187d2c3ef79f140b2fb147c
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 15:47:00 +0200

    Update IPeliculaDao.java

--- UD25/Ex4/src/main/java/com/ejercicio4/dao/IPeliculaDao.java
+++ UD25/Ex4/src/main/java/com/ejercicio4/dao/IPeliculaDao.java
@@ -1,17 +1,8 @@
-/**
- * 
- */
 package com.ejercicio4.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ejercicio4.dto.Pelicula;
 
-
-/**
- * @author Miguel A. Sastre
- *
- */
 public interface IPeliculaDao extends JpaRepository<Pelicula, Long> {
 
 }
