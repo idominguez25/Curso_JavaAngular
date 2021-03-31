ddf67d1d09a67643a663e8d0e31598a2971eb7f2
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 16:02:09 +0200

    Update PiezaController.java

--- UD26/Ex1/src/main/java/com/ejercicio1/controller/PiezaController.java
+++ UD26/Ex1/src/main/java/com/ejercicio1/controller/PiezaController.java
@@ -1,20 +1,10 @@
-/**
- * 
- */
 package com.ejercicio1.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import com.ejercicio1.dto.Pieza;
 import com.ejercicio1.service.PiezaServiceImpl;
-
-/**
- * @author Miguel A. Sastre
- *
- */
 
 @RestController
 @RequestMapping("/api")
