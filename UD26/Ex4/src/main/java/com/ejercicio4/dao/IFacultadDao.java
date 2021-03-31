5bc8ad5d72d0a62c7e9046af9a68099d6466f547
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 18:58:33 +0200

    Update IFacultadDao.java

--- UD26/Ex4/src/main/java/com/ejercicio4/dao/IFacultadDao.java
+++ UD26/Ex4/src/main/java/com/ejercicio4/dao/IFacultadDao.java
@@ -1,17 +1,8 @@
-/**
- * 
- */
 package com.ejercicio4.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ejercicio4.dto.Facultad;
 
-
-/**
- * @author Miguel A. Sastre
- *
- */
 public interface IFacultadDao extends JpaRepository<Facultad, Long> {
 
 }
