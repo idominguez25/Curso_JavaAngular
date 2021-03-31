f8a80428959e2a800f23330b5dc12411ac2f5b7b
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 17:38:58 +0200

    Update IMaquinaRegistradoraDao.java

--- UD26/Ex3/src/main/java/com/ejercicio3/dao/IMaquinaRegistradoraDao.java
+++ UD26/Ex3/src/main/java/com/ejercicio3/dao/IMaquinaRegistradoraDao.java
@@ -1,17 +1,8 @@
-/**
- * 
- */
 package com.ejercicio3.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ejercicio3.dto.MaquinaRegistradora;
 
-
-/**
- * @author Miguel A. Sastre
- *
- */
 public interface IMaquinaRegistradoraDao extends JpaRepository<MaquinaRegistradora, Long> {
 
 }
