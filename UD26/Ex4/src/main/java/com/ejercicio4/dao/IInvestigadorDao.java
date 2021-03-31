e22e440fe1ce2eaef054601b8c8c20c3e55ef79e
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 18:58:47 +0200

    Update IInvestigadorDao.java

--- UD26/Ex4/src/main/java/com/ejercicio4/dao/IInvestigadorDao.java
+++ UD26/Ex4/src/main/java/com/ejercicio4/dao/IInvestigadorDao.java
@@ -1,17 +1,8 @@
-/**
- * 
- */
 package com.ejercicio4.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ejercicio4.dto.Investigador;
 
-
-/**
- * @author Miguel A. Sastre
- *
- */
 public interface IInvestigadorDao extends JpaRepository<Investigador, String> {
 
 }
