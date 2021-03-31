2b3d90c13044e4dbc93b3ef74a62c33fb4868946
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2021-03-31 15:21:37 +0200

    Update IDepartamentoDao.java

--- UD25/Ex2/src/main/java/com/ejercicio2/dao/IDepartamentoDao.java
+++ UD25/Ex2/src/main/java/com/ejercicio2/dao/IDepartamentoDao.java
@@ -1,17 +1,8 @@
-/**
- * 
- */
 package com.ejercicio2.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ejercicio2.dto.Departamento;
 
-
-/**
- * @author Miguel A. Sastre
- *
- */
 public interface IDepartamentoDao extends JpaRepository<Departamento, Long> {
 
 }
